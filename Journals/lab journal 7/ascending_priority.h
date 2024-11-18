2a381f98d04b3c2755d26e123d654d4ba2a1f988
Ares46 <157149470+Ares46@users.noreply.github.com>
2024-11-18 13:35:29 +0500

    Add files via upload

---
+++ Journals/lab journal 7/ascending_priority.h
@@ -0,0 +1,17 @@
+#include<iostream>
+#include "node.h"
+using namespace std;
+class ascending_priority
+{
+       node* head;
+	   node* front;
+	   node* rear;
+public:
+	ascending_priority();
+	void Enqueue(int a);
+	void Dequeue();
+
+	ascending_priority();
+	~ascending_priority();
+};
+
---
+++ Journals/lab journal 7/node.h
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class node
+{
+
+
+public:
+	int val;
+	node* next;
+	node();
+	node(int);
+	
+	~node();
+	
+};
+
